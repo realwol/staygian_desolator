4bb799747583acfe5b5fb27a3203704412a1a700
gaara <realwol@gmail.com>
2016-02-25 15:15:42 +0800

    fix link_from

--- app/models/shop_link.rb
+++ app/models/shop_link.rb
@@ -2,6 +2,6 @@ class ShopLink < ActiveRecord::Base
   belongs_to :user
 
   scope :un_grasp, -> {where(status: 'false')}
-  scope :from_search, -> {where(status: 'search')}
-  scope :from_shop, -> {where(status: 'shop')}
+  scope :from_search, -> {where(link_from: 'search')}
+  scope :from_shop, -> {where(link_from: 'shop')}
 end
