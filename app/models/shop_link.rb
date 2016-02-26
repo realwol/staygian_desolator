8e045e0f33d7df74118b9e4c91a74a324614b5c7
gaara <realwol@gmail.com>
2016-02-26 11:13:19 +0800

    change need retry

--- app/models/shop_link.rb
+++ app/models/shop_link.rb
@@ -4,4 +4,5 @@ class ShopLink < ActiveRecord::Base
   scope :un_grasp, -> {where(status: 'false')}
   scope :from_search, -> {where(link_from: 'search')}
   scope :from_shop, -> {where(link_from: 'shop')}
+  scope :need_retry, -> {where(link_retry: true)}
 end
