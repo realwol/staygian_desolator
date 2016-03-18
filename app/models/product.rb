eb8d42facf4f7104011dc56a992261381bfb26d2
gaara <realwol@gmail.com>
2016-03-18 19:22:56 +0800

    fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -76,7 +76,7 @@ class Product < ActiveRecord::Base
   	@valid_images
   end
 
-  def export_format_string string
+  def self.export_format_string string
     string.strip.gsub('，', ',').gsub('。', '.').gsub("\r", '') if string.present?
   end
 
@@ -85,53 +85,53 @@ class Product < ActiveRecord::Base
     product_translation = product.product_info_translations.last
     case language
     when 'british'
-      local_infos[:title] = export_format_string(product_translation.try(:e_t))
-      local_infos[:detail] = export_format_string(product_translation.try(:e_detail))
-      local_infos[:des1] = export_format_string(product_translation.try(:e_des1))
-      local_infos[:des2] = export_format_string(product_translation.try(:e_des2))
-      local_infos[:des3] = export_format_string(product_translation.try(:e_des3))
+      local_infos[:title] = Product.export_format_string(product_translation.try(:e_t))
+      local_infos[:detail] = Product.export_format_string(product_translation.try(:e_detail))
+      local_infos[:des1] = Product.export_format_string(product_translation.try(:e_des1))
+      local_infos[:des2] = Product.export_format_string(product_translation.try(:e_des2))
+      local_infos[:des3] = Product.export_format_string(product_translation.try(:e_des3))
     when 'germany'
-      local_infos[:title] = export_format_string(product_translation.try(:g_t))
-      local_infos[:detail] = export_format_string(product_translation.try(:g_detail))
-      local_infos[:des1] = export_format_string(product_translation.try(:g_des1))
-      local_infos[:des2] = export_format_string(product_translation.try(:g_des2))
-      local_infos[:des3] = export_format_string(product_translation.try(:g_des3))
+      local_infos[:title] = Product.export_format_string(product_translation.try(:g_t))
+      local_infos[:detail] = Product.export_format_string(product_translation.try(:g_detail))
+      local_infos[:des1] = Product.export_format_string(product_translation.try(:g_des1))
+      local_infos[:des2] = Product.export_format_string(product_translation.try(:g_des2))
+      local_infos[:des3] = Product.export_format_string(product_translation.try(:g_des3))
     when 'france'
-      local_infos[:title] = export_format_string(product_translation.try(:f_t))
-      local_infos[:detail] = export_format_string(product_translation.try(:f_detail))
-      local_infos[:des1] = export_format_string(product_translation.try(:f_des1))
-      local_infos[:des2] = export_format_string(product_translation.try(:f_des2))
-      local_infos[:des3] = export_format_string(product_translation.try(:f_des3))
+      local_infos[:title] = Product.export_format_string(product_translation.try(:f_t))
+      local_infos[:detail] = Product.export_format_string(product_translation.try(:f_detail))
+      local_infos[:des1] = Product.export_format_string(product_translation.try(:f_des1))
+      local_infos[:des2] = Product.export_format_string(product_translation.try(:f_des2))
+      local_infos[:des3] = Product.export_format_string(product_translation.try(:f_des3))
     when 'spain'
-      local_infos[:title] = export_format_string(product_translation.try(:s_t))
-      local_infos[:detail] = export_format_string(product_translation.try(:s_detail))
-      local_infos[:des1] = export_format_string(product_translation.try(:s_des1))
-      local_infos[:des2] = export_format_string(product_translation.try(:s_des2))
-      local_infos[:des3] = export_format_string(product_translation.try(:s_des3))
+      local_infos[:title] = Product.export_format_string(product_translation.try(:s_t))
+      local_infos[:detail] = Product.export_format_string(product_translation.try(:s_detail))
+      local_infos[:des1] = Product.export_format_string(product_translation.try(:s_des1))
+      local_infos[:des2] = Product.export_format_string(product_translation.try(:s_des2))
+      local_infos[:des3] = Product.export_format_string(product_translation.try(:s_des3))
     when 'italy'
-      local_infos[:title] = export_format_string(product_translation.try(:i_t))
-      local_infos[:detail] = export_format_string(product_translation.try(:i_detail))
-      local_infos[:des1] = export_format_string(product_translation.try(:i_des1))
-      local_infos[:des2] = export_format_string(product_translation.try(:i_des2))
-      local_infos[:des3] = export_format_string(product_translation.try(:i_des3))
+      local_infos[:title] = Product.export_format_string(product_translation.try(:i_t))
+      local_infos[:detail] = Product.export_format_string(product_translation.try(:i_detail))
+      local_infos[:des1] = Product.export_format_string(product_translation.try(:i_des1))
+      local_infos[:des2] = Product.export_format_string(product_translation.try(:i_des2))
+      local_infos[:des3] = Product.export_format_string(product_translation.try(:i_des3))
     when 'america'
-      local_infos[:title] = export_format_string(product_translation.try(:e_t))
-      local_infos[:detail] = export_format_string(product_translation.try(:e_detail))
-      local_infos[:des1] = export_format_string(product_translation.try(:e_des1))
-      local_infos[:des2] = export_format_string(product_translation.try(:e_des2))
-      local_infos[:des3] = export_format_string(product_translation.try(:e_des3))
+      local_infos[:title] = Product.export_format_string(product_translation.try(:e_t))
+      local_infos[:detail] = Product.export_format_string(product_translation.try(:e_detail))
+      local_infos[:des1] = Product.export_format_string(product_translation.try(:e_des1))
+      local_infos[:des2] = Product.export_format_string(product_translation.try(:e_des2))
+      local_infos[:des3] = Product.export_format_string(product_translation.try(:e_des3))
     when 'canada'
-      local_infos[:title] = export_format_string(product_translation.try(:e_t))
-      local_infos[:detail] = export_format_string(product_translation.try(:e_detail))
-      local_infos[:des1] = export_format_string(product_translation.try(:e_des1))
-      local_infos[:des2] = export_format_string(product_translation.try(:e_des2))
-      local_infos[:des3] = export_format_string(product_translation.try(:e_des3))
+      local_infos[:title] = Product.export_format_string(product_translation.try(:e_t))
+      local_infos[:detail] = Product.export_format_string(product_translation.try(:e_detail))
+      local_infos[:des1] = Product.export_format_string(product_translation.try(:e_des1))
+      local_infos[:des2] = Product.export_format_string(product_translation.try(:e_des2))
+      local_infos[:des3] = Product.export_format_string(product_translation.try(:e_des3))
     else
-      local_infos[:title] = export_format_string(product.title)
-      local_infos[:detail] = export_format_string(product.details)
-      local_infos[:des1] = export_format_string(product.product_intro_1)
-      local_infos[:des2] = export_format_string(product.product_intro_2)
-      local_infos[:des3] = export_format_string(product.product_intro_3)
+      local_infos[:title] = Product.export_format_string(product.title)
+      local_infos[:detail] = Product.export_format_string(product.details)
+      local_infos[:des1] = Product.export_format_string(product.product_intro_1)
+      local_infos[:des2] = Product.export_format_string(product.product_intro_2)
+      local_infos[:des3] = Product.export_format_string(product.product_intro_3)
     end
     local_infos
   end
