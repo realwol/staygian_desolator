57bcc5483c5125abf1a888d2cfe3e4403149baeb
gaara <realwol@gmail.com>
2016-07-07 17:07:53 +0800

    add applicaiton helper for mem ana

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  around_filter :record_memory if Rails.env.production?
 
   def selected_user
     @selected_user = session[:selected_user_id].present? ? User.find(session[:selected_user_id]) : current_user
     @selected_user
+  end
+
+  def record_memory
+    process_status = File.open("/proc/#{Process.pid}/status")
+    13.times {process_status.get}
+    res_before_action = process_status.gets.split[1].to_i
+    process_status.close
+    yield
+    process_status = File.open("/proc/#{Process.pid}/status")
+    13.times {process_status.get}
+    res_after_action = process_status.gets.split[1].to_i
+    process_status.close
+    logger.info("CONSUME MEMORY: #{res_after_action - res_before_action} KB\t NOW #{res_after_action} KB\t #{request.url}")
   end
 end
